98d00f9dd0debe7507ab835b0a504d517d31316d
ogawaluan <ogawa.luan@gmail.com>
2023-04-29 10:59:12 -0300

    feat: created authFlow&resetPassword

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -11,6 +11,8 @@ DeviseTokenAuth.setup do |config|
   # determines how long tokens will remain valid after they are issued.
   config.token_lifespan = 1.weeks
 
+  config.require_client_password_reset_token = true
+
   # Limiting the token_cost to just 4 in testing will increase the performance of
   # your test suite dramatically. The possible cost value is within range from 4
   # to 31. It is recommended to not use a value more than 10 in other environments.
